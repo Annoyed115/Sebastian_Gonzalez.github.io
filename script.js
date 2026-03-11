7c4c813589da6813641dc0bf3b5d3b52f87d99c5
Sebastian Gonzalez <sebas.nice12@gmail.com>
2026-03-11 12:20:32 -0500

    Animations V1.0

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-// const table = document.querySelector('.table_1');
+const table = document.querySelector('.table_1');
 
-// anime ({
-//     targets: '.table_1',
-//     translateY: [-300, 0],
-//     rotate: [
-//       { value: -10, duration: 800, easing: 'easeInOutSine' },
-//       { value: 10, duration: 800, easing: 'easeInOutSine' }
-//     ],
-//     loop: true,
-//     direction: 'alternate'
-    
-// });
-
-const tl = anime.timeline({});
-
-tl.add({
-   targets: '.table_1',
-    translateY: [-300, 0, -30, 0],
-    rotate: [0, 0, -3, 3, -1, 1, 0],
-    duration: 3000,
+anime ({
+    targets: '.table_1',
+    translateY: [-400, 0],
+    duration: 3400,
     easing: 'easeOutElastic(1, 0.5)'
-})
-tl.add({
     
 });
+
